c2081ae6d4d544f68d76ae324199065d6aea02a6
Levi Thieme <thielt01@pfw.edu>
2019-12-07 18:49:40 -0500

    update socket server implementation

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-const WebSocket = require('ws');
-const port = process.env.PORT || 8080;
-const wss = new WebSocket.Server({ port: port });
+const express = require("express");
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
 
-wss.on('connection', function connection(ws) {
-    let data = {};
-    ws.on('message', function incoming(message) {
-        data = JSON.parse(message);
-        console.log(data);
-    });
-    data.message = "Hi";
-    ws.send(JSON.stringify(data));
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const { Server } = require('ws');
+const wss = new Server({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
 });
+
+setInterval(() => {
+  wss.clients.forEach((client) => client.send({ message: new Date().toTimeString() }))
+}, 1000);

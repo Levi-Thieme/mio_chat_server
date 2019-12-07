a7aab934ca8896efc959c1948c6f3eff3594c47e
Levi Thieme <thielt01@pfw.edu>
2019-12-07 18:28:07 -0500

    addded http server.

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ wss.on('connection', function connection(ws) {
     data.message = "Hi";
     ws.send(JSON.stringify(data));
 });
+
+const http = require('http')
+
+const hostname = 'localhost'
+const port = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Hello World\n')
+})
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
